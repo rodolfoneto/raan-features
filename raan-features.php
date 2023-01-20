acb6c8e8e6f37ede3bea69217b6ceea9294193b2
Rodolfo Neto <rodolfoneto>
2023-01-20 12:14:55 -0800

    first commit

---
+++ raan-features.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Plugin Name: Raan Features
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: rodolfoneto
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
+
+add_action('wp_body_open', function(){
+    echo '<div class="content">RODOLFO</div>';
+});
